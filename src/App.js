7151e48916f468c5e18c8fbe37929be08212306c
wrightunik <wrightunik23@gmail.com>
2023-05-30 21:49:41 -0400

    added some stuff toabout me

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import About from "./components/About.js"
 
 function App() {
   return (
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import "./About.css"
+
+export const About = () => {
+    return (
+    <section className="intro">
+<p>
+Welcome to our team's "About Me" page! Here you'll find an introduction to each member of our talented and dedicated team. 
+
+Our team consist of four briliant aspiring software engineers, Silis Kleemoff, Wai Leong Chong, Angie Diaz, and Kinu Wright. 
+
+</p>
+    </section>
+    )
+}
