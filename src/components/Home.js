0f660428f1f281233b470a49ef5c436bfd10db67
Silis Kleemoff <99054066+KleemoffDeveloper@users.noreply.github.com>
2023-06-06 22:00:17 -0400

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,20 @@
 import VideoCard from './Cards/VideoCard';
 import './Home.css'
-
-export default function Home({videos = []}){
+import ModalWindow from './ModalWindow';
+export default function Home({videos}){
     return (
         <div className="home">
             <ul className="card-list"> 
-                {videos.map((video) => (
-                    <VideoCard
-                        data={video}
-                        key={video.id.videoId}                    
-                    />    
-                ))}
+            {/* 
+                If there are videos, they will display on the webpage
+                If not, display an error message
+                It's using a ternary operator
+            */}
+                {videos ? videos.map(video => <VideoCard data={video}/>) : (
+                    <div>
+                        <ModalWindow/>
+                    </div>
+                )}
             </ul>
         </div>
     );
